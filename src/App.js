567dd821e3407a7707769f64f2ff04f5db5aa8d2
DipokSarker <dipok.ronjan.sarker@gmail.com>
2023-03-27 23:59:40 +0300

    state added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import { useState } from 'react';
+
 function App() {
-    return <div>Show animal list Here!</div>
+    const [count, setCount] = useState(0);
+
+    const handleClick = () => {
+        setCount(count + 1);
+    };
+
+
+    return (
+    <div>
+        <button onClick={handleClick}>Add Animal</button>
+        <div>Number of animals: {count}</div>
+    </div>
+    );
 }
 
 
