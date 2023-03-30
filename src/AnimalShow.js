10dad9d5e84f69806316ad2efb413b19b1f1b34d
DipokSarker <dipok.ronjan.sarker@gmail.com>
2023-03-31 00:36:37 +0300

    click handler added

--- src/AnimalShow.js
+++ src/AnimalShow.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import bird from './svg/bird.svg';
 import cat from './svg/cat.svg';
 import cow from './svg/cow.svg';
@@ -6,8 +7,32 @@ import gator from './svg/gator.svg';
 import heart from './svg/heart.svg';
 import horse from './svg/horse.svg';
 
+const svgMap = {
+    bird,
+    cat,
+    cow,
+    dog,
+    gator,
+    horse
+};
+
 function AnimalShow( {type} ) {
-    return <div>{type}</div>;
+    const [clicks, setClicks] = useState(0);
+
+    const handleClick = () => {
+        setClicks(clicks + 1);
+    };
+
+    return (
+        <div onClick={handleClick}>
+        <img alt="animal" src={svgMap[type]} />
+        <img 
+        alt="heart" 
+        src={heart} 
+        style={{ width: 10 + 10 * clicks + 'px'}}
+        />
+    </div>
+    );
 }
 
 export default AnimalShow;  
